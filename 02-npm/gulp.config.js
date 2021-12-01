3a57984662973411a6e0666f60231c7f23c0bcf7
xyySys <13860539310@163.com>
2021-12-01 16:50:29 +0800

    gulp使用

--- 02-npm/gulp.config.js
+++ 02-npm/gulp.config.js
@@ -1,15 +1,17 @@
 const {
-    src,
-    dest
+    series, //让任务（task）按顺序执行
+    parallel //以最大并发来运行的任务（tasks）
 } = require('gulp')
-
-function scripts() {
-    return src('./demos/01-buildings/app.js', {
-            sourcemaps: true
-        })
-        .pipe(dest('./demos/dist/'));
+const s1 = (cb) => {
+    cb()
 }
+const s2 = cb => {
+    cb()
+}
+const node_env=process.env.NODE_ENV
+console.log(node_env)
 
-var build = gulp.series(scripts);
+exports.default = series(s1, s2);
 
-exports.default = build
+
+//根目录运行npx gulp -f gulp.config.js
---
+++ 02-npm/gulpfile.js
@@ -0,0 +1,15 @@
+const {
+    series, //让任务（task）按顺序执行
+    parallel //以最大并发来运行的任务（tasks）
+} = require('gulp')
+const s1 = (cb) => {
+    cb()
+}
+const s2 = cb => {
+    cb()
+}
+exports.default = series(s1, s2);
+
+
+
+//根目录运行npx gulp -f gulpfile.js
